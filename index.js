f6a26927e13eed1b42da779c309bd62b6f1d0513
Alejo Vicino <vicinoalejog@gmail.com>
2022-10-18 22:08:53 -0300

    2da entrega del proyecto

--- index.js
+++ index.js
@@ -4,18 +4,58 @@ let nomCliente = prompt("***COTIZADOR IMPRESIONES 3D***\n Ingresa nombre de clie
 
 console.log("Cliente: "+nomCliente);
 
+
+let clientes = "";
+for  (let index = 0; index < 1; index++){
+    clientes += prompt("Cliente n°: ")+"\n";
+}
+
+console.log("Cliente n° "+clientes);
 //Ingresamos datos para saber el costo del producto a imprimir
 
 function cotizar(){
-    let num1=parseInt(prompt("Ingresa precio (ARS) del filamento"));
-    let num2=parseInt(prompt("Ingresar peso (g) bobina completa de filamento"));
-    let num3=parseInt(prompt("Ingresa peso en gramos del producto"));
-    let resultado=num1/num2*num3;
+    let precioFilamento=parseInt(prompt("Ingresa precio (ARS) del filamento"));
+    let pesoBobina=parseInt(prompt("Ingresar peso (g) bobina completa de filamento"));
+    let pesoProducto=parseInt(prompt("Ingresa peso en gramos del producto"));
+    let resultado=precioFilamento/pesoBobina*pesoProducto;
     console.log("El precio de costo es: "+resultado+" ARS");
 }
 
 cotizar();
 
+// COTIZACION A DOLARES 
 
+const COTIZACION_DOLAR = 290;
+const cotizarDolar = (pesos) => pesos / COTIZACION_DOLAR;
 
+let seleccion = prompt("¿Cotizar el valor en dolares? \n si \n no \n Ingrese si o no segun corresponda");
+switch (seleccion){
+    case "si":
+        let valor = prompt("Ingresar valor en pesos");
+        alert("El precio en dolares es: "+cotizarDolar(valor)+" USD");
+        break;
+    case "no":
+        alert("Valor sin cotizar");
+        break;
+    default:
+        break;
+}
+
+// CLientes 
+
+class Cliente{
+    constructor(nombre, numCliente, numTelefono, empresa){
+        this.nombre = nombre;
+        this.numCliente = numCliente;
+        this.numTelefono = numTelefono;
+        this.empresa = empresa;
+    }
+}
+
+const cliente1 = new Cliente("Guillermo Mercado", "1", "1558471256","Mercado3d");
+const cliente2 = new Cliente("Fernando Perez", "2", "1569585472","Perez3d");
+const cliente3 = new Cliente("Ivan Lopez", "3", "1532759426","Lopez3d");
+console.log(cliente1);
+console.log(cliente2);
+console.log(cliente3);
 
